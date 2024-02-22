47b8217b5153c24f19816c625d05e43649a58233
AZKDOROVJDK332 <143708922+AZKDOROVJDK332@users.noreply.github.com>
2024-02-22 20:06:36 +0300

    Update GURU-gemini.js

--- plugins/GURU-gemini.js
+++ plugins/GURU-gemini.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI('AIzaSyBWozNQdyPr6q5D7U1Izfl3BArjnNfwGuA');
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
-    if (!text) throw `uhm.. what do you want to say?`
+    if (!text) throw امم..ماذا تريد أن تقول?`
     m.react('🤖')
     await displayLoadingScreen(conn, m.chat)
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
@@ -18,11 +18,11 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
     m.reply(textt)
   } catch (error) {
     console.error(error);
-    m.reply('Oops! Something went wrong. , we are trying had to fix it asap');
+    m.reply('أُووبس! هناك خطأ ما. ، ونحن نحاول إصلاحه في أسرع وقت ممكن');
   }
 }
-handler.help = ['gemini <text>']
+handler.help = ['توأم+النص']
 handler.tags = ['tools']
-handler.command = /^(gemini)$/i
+handler.command = /^توأم$/i
 
 export default handler
